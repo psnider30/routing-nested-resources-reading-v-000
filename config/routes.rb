2c7e9ae77a9341112f542c83b2162779ee7ba500
psnider30 <pbsnider@gmail.com>
2017-09-21 12:08:22 -0700

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do
+    #nested resources for posts
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  # get 'authors/:id/posts', to: 'authors#posts_index'
+  # get 'authors/:id/posts/:post_id', to: 'authors#post', as:'author_post'
   root 'posts#index'
 end
